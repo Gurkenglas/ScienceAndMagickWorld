311fea52a46f6659aabd24917f6e92171daa9561
glassesofdoom <Glassesofdoom@gmail.com>
2013-09-24 18:46:44 -0400

    ids and assets

--- samw_common/us/samcraft/samw/lib/ItemID.java
+++ samw_common/us/samcraft/samw/lib/ItemID.java
@@ -1,21 +1,33 @@
 package us.samcraft.samw.lib;
 
 public class ItemID {
-	
-	/*Default Item IDs*/
+
+	/* Default Item IDs */
 	public static int GEM_DEFAULT = 4500;
 	public static int MOLD_DEFAULT = 4501;
 	public static int SIMPTOOL_DEFAULT = 4502;
 	public static int AXEHEAD_DEFAULT = 4503;
 	public static int BLADE_DEFAULT = 4504;
 	public static int SHOVELHEAD_DEFAULT = 4505;
-	
+	public static int ADAMANTIUM_INGOT_DEFAULT = 4506;
+	public static int BRONZE_INGOT_DEFAULT = 4507;
+	public static int COPPER_INGOT_DEFAULT = 4508;
+	public static int MANA_INGOT_DEFAULT = 4509;
+	public static int MITHRIL_INGOT_DEFAULT = 4510;
+	public static int ORICHALCUM_INGOT_DEFAULT = 4511;
+	public static int RUNE_INGOT_DEFAULT = 4512;
+	public static int SILVER_INGOT_DEFAULT = 4513;
+	public static int STARSTEEL_INGOR_DEFAULT = 4514;
+	public static int STEEL_INGOT_DEFAULT = 4515;
+	public static int TITANIUM_INGOT_DEFAULT = 4516;
+	public static int PICKAXE_DEFAULT = 4517;
 
-	/*Current Item IDs*/
+	/* Current Item IDs */
 	public static int GEM;
 	public static int MOLD;
 	public static int SIMPTOOL;
 	public static int AXEHEAD;
 	public static int BLADE;
 	public static int SHOVELHEAD;
+	public static int PICKAXE;
 }
