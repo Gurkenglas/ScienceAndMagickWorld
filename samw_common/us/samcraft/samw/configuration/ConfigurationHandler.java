ca3fe13c1cb32ac9cb8bc709c144508fad3b1c81
glassesofdoom <Glassesofdoom@gmail.com>
2013-09-24 22:25:44 -0400

    Drep forgot ids we're shifted

--- samw_common/us/samcraft/samw/configuration/ConfigurationHandler.java
+++ samw_common/us/samcraft/samw/configuration/ConfigurationHandler.java
@@ -3,13 +3,10 @@ package us.samcraft.samw.configuration;
 import java.io.File;
 import java.util.logging.Level;
 
-import us.samcraft.samw.lib.INames;
-import us.samcraft.samw.lib.ItemID;
-import us.samcraft.samw.lib.Reference;
-
-import cpw.mods.fml.common.FMLLog;
-
 import net.minecraftforge.common.Configuration;
+import us.samcraft.samw.lib.ITNames;
+import us.samcraft.samw.lib.ItemID;
+import cpw.mods.fml.common.FMLLog;
 
 
 public class ConfigurationHandler {
@@ -24,12 +21,13 @@ public class ConfigurationHandler {
 			 
 			configuration.load();
 			 
-			ItemID.GEM = configuration.getItem(Configuration.CATEGORY_ITEM, INames.GEM, ItemID.GEM_DEFAULT).getInt();
-			ItemID.MOLD = configuration.getItem(Configuration.CATEGORY_ITEM, INames.MOLD, ItemID.MOLD_DEFAULT).getInt();
-			ItemID.BLADE = configuration.get(Configuration.CATEGORY_ITEM, INames.BLADE, ItemID.BLADE_DEFAULT).getInt();
-			ItemID.SIMPTOOL = configuration.getItem(Configuration.CATEGORY_ITEM, INames.SIMPTOOL, ItemID.SIMPTOOL_DEFAULT).getInt();
-			ItemID.AXEHEAD = configuration.getItem(Configuration.CATEGORY_ITEM, INames.AXEHEAD, ItemID.AXEHEAD_DEFAULT).getInt();
-			ItemID.SHOVELHEAD = configuration.getItem(Configuration.CATEGORY_ITEM, INames.SHOVHEAD, ItemID.SHOVELHEAD_DEFAULT).getInt();
+			ItemID.GEM = configuration.getItem(Configuration.CATEGORY_ITEM, ITNames.GEM, ItemID.GEM_DEFAULT).getInt() - 256;
+			ItemID.MOLD = configuration.getItem(Configuration.CATEGORY_ITEM, ITNames.MOLD, ItemID.MOLD_DEFAULT).getInt() - 256;
+			ItemID.BLADE = configuration.get(Configuration.CATEGORY_ITEM, ITNames.BLADE, ItemID.BLADE_DEFAULT).getInt();
+			ItemID.SIMPTOOL = configuration.getItem(Configuration.CATEGORY_ITEM, ITNames.SIMPTOOL, ItemID.SIMPTOOL_DEFAULT).getInt() - 256;
+			ItemID.AXEHEAD = configuration.getItem(Configuration.CATEGORY_ITEM, ITNames.AXEHEAD, ItemID.AXEHEAD_DEFAULT).getInt() - 256;
+			ItemID.SHOVELHEAD = configuration.getItem(Configuration.CATEGORY_ITEM, ITNames.SHOVHEAD, ItemID.SHOVELHEAD_DEFAULT).getInt() - 256;
+			ItemID.PICKAXEHEAD = configuration.getItem(Configuration.CATEGORY_ITEM, ITNames.PAXEHEAD, ItemID.PICKAXEHEAD_DEFAULT).getInt() - 256;
 			 
 		 }catch(Exception e) {
 			 FMLLog.log(Level.SEVERE, e, "Science&MagickWorld" + "Has had a issue loading its config");
