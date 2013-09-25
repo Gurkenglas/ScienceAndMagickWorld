c1ae30696ff5ee057cb8dd0da25359bcbbefc7cd
glassesofdoom <Glassesofdoom@gmail.com>
2013-09-24 22:28:57 -0400

    Forgot to add

---
+++ samw_common/us/samcraft/samw/item/ItemPickAxeHead.java
@@ -0,0 +1,51 @@
+package us.samcraft.samw.item;
+
+import java.util.List;
+
+import us.samcraft.samw.SAMWMain;
+import us.samcraft.samw.lib.ITNames;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+
+public class ItemPickAxeHead extends Item{
+	
+	@SideOnly(Side.CLIENT)
+	private Icon[] icons;
+
+	public ItemPickAxeHead(int id){
+		super(id);
+		this.setCreativeTab(SAMWMain.tabsamw);
+		this.setHasSubtypes(true);
+	}
+	@Override
+	public String getUnlocalizedName(ItemStack itemstack){
+		return ITNames.PAXEHEAD + itemstack.getItemDamage();
+	}
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister reg){
+		icons = new Icon[ITNames.PAXHEAD_ICONS.length];
+		for (int i = 0; i < icons.length; i++){
+			icons[i] = reg.registerIcon("samw:parts/pickaxe/" + ITNames.PAXHEAD_ICONS[i]);
+		}
+	}
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIconFromDamage(int dmg){
+		return icons[dmg];
+	}
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(int id, CreativeTabs tabs, List list){
+		for (int i = 0; i < ITNames.BLADE_META_NAME.length; i++){
+			ItemStack stack = new ItemStack(id, 1, i);
+			list.add(stack);
+		}
+	}
+
+}
