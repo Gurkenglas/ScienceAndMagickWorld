5d8fae04d40e81f6c23236c11abc4944103f7ddf
Glassesofdoom <glassesofdoom@gmail.com>
2013-09-26 08:38:00 -0400

    Moved assets to a different directory and corrected a mistake in ModItemIcons. Lets begin the all day push XD

--- samw_common/us/samcraft/samw/lib/textures/ModItemIcons.java
+++ samw_common/us/samcraft/samw/lib/textures/ModItemIcons.java
@@ -5,6 +5,6 @@ public class ModItemIcons {
 	private static final String PARTS = "samw:parts/";
 	private static final String INGOTS = "samw:ingots/";
 	private static final String GEMS = "samw:gems/";
-	private static final String MOLD = "samw:parts";
-	
+	private static final String MOLD = "samw:molds/";
+	private static final String SIMPLETOOL ="samw:simpletool/";
 }
