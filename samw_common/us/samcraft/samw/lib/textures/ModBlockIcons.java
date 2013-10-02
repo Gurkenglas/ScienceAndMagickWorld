1b45554057bda3595ea7f8d8a1302f23b59b0d1c
Glassesofdoom <glassesofdoom@gmail.com>
2013-10-02 13:54:51 -0400

    Working on Block textures should have done this first

---
+++ samw_common/us/samcraft/samw/lib/textures/ModBlockIcons.java
@@ -0,0 +1,11 @@
+package us.samcraft.samw.lib.textures;
+
+public class ModBlockIcons {
+	
+	private static final String ORE = "samw:ore/";
+	private static final String TREE = "samw:tree/";
+	
+	public static final String AdamantiumOre = ORE + "Adamantium";
+	
+
+}
