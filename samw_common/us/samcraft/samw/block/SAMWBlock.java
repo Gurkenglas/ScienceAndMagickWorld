678023c7b55712405100124aa0e29dec431a2e0f
glassesofdoom <Glassesofdoom@gmail.com>
2013-10-02 00:33:02 -0400

    Got started on blocks need to finish

--- samw_common/us/samcraft/samw/block/SAMWBlock.java
+++ samw_common/us/samcraft/samw/block/SAMWBlock.java
@@ -7,7 +7,7 @@ public class SAMWBlock {
 	
 	public static Block oreAdamantium;
 	public static Block oreCopper;
-	public static Block oremMithril;
+	public static Block oreMithril;
 	public static Block oreOrichalcum;
 	public static Block oreRune;
 	public static Block oreSilicon;
@@ -18,6 +18,15 @@ public class SAMWBlock {
 	public static void init(){
 		
 		/*Ores*/
-		//oreAdamantium = new BlockoreAdamantium(BlockID.ADAMANTUM_ORE);
+		oreAdamantium = new BlockoreAdamantium(BlockID.ADAMANTUM_ORE);
+		oreCopper = new BlockoreCoppere(BlockID.COPPER_ORE);
+		oreMithril = new BlockoreMithril(BlockID.MITHRIL_ORE);
+		oreOrichalcum = new BlockoreOrichalcum(BlockID.ORICHALCUM_ORE);
+		oreRune = new BlockoreRune(BlockID.RUNE_ORE);
+		oreSilicon = new BlockoreSilicon(BlockID.SILICON_ORE);
+		oreSiliver = new BlockoreSiliver(BlockID.SILIVER_ORE_DEFAULT); 
+		oreTitanium = new BlockoreTitanium(BlockID.SILIVER_ORE);
+		oreWonderflonium = new BlockoreWonderflonium(BlockID.WONDERFLONIUM_ORE);
+		// TODO work on blocks 
 	}
 }
