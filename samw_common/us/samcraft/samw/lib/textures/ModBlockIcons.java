3a0e84ff4a13c68b3e6481c0391afc09602deb17
glassesofdoom <Glassesofdoom@gmail.com>
2013-10-05 00:21:38 -0400

    Another bad place holder texture need to fix these -_-'

--- samw_common/us/samcraft/samw/lib/textures/ModBlockIcons.java
+++ samw_common/us/samcraft/samw/lib/textures/ModBlockIcons.java
@@ -5,7 +5,17 @@ public class ModBlockIcons {
 	private static final String ORE = "samw:ore/";
 	private static final String TREE = "samw:tree/";
 	
-	public static final String AdamantiumOre = ORE + "Adamantium";
+	public static final String oreAdamantium = ORE + "Adamantium";
+	public static final String oreCopper = ORE + "Copper";
+	public static final String oreMithril = ORE + "Mithril";
+	public static final String oreOrichalcum = ORE + "Orichalcum";
+	public static final String oreRune = ORE + "Rune";
+	public static final String oreSilicon = ORE + "Silicon";
+	public static final String oreSilver = ORE + "Silver";
+	public static final String oreStarsteel = ORE + "Starsteel";
+	public static final String oreTin = ORE + "Tin";
+	public static final String oreTitanium = ORE + "Titanium";
+	public static final String oreWonderflonium = ORE + "Wonderflonium";
 	
-
+	public static final String Limestone = ORE + "Limeststone";
 }
