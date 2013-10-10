f01a2a4e77742206ccbb5c0176056d125c7aad80
glassesofdoom <Glassesofdoom@gmail.com>
2013-10-10 14:27:40 -0400

    Unproductive day

--- samw_common/us/samcraft/samw/crafting/TempRecipes.java
+++ samw_common/us/samcraft/samw/crafting/TempRecipes.java
@@ -1,9 +1,21 @@
 package us.samcraft.samw.crafting;
 
+import us.samcraft.samw.item.SAMWItem;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.registry.GameRegistry;
+
 public class TempRecipes {
 
 	public static void init() {
-		// TODO Auto-generated method stub
+		
+		/* There should be a way to loop through these meta items I'll do it later*/
+		AxeHead();
+		
+	}
+
+	private static void AxeHead() {
+
+		GameRegistry.addShapedRecipe(new ItemStack(SAMWItem.AxeHead, 1), new Object[]{ " xx", "  X", "   ", 'x', SAMWItem.AdamantiumIngot });
 		
 	}
 
