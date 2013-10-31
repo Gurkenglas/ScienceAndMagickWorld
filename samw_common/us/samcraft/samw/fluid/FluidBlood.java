4fdeb468cfa0c60ca7332dcfe9364af53e7234fc
LordIllyohs <lordillyohs@gmail.com>
2013-10-31 18:00:48 -0400

    kind of hurts

--- samw_common/us/samcraft/samw/fluid/FluidBlood.java
+++ samw_common/us/samcraft/samw/fluid/FluidBlood.java
@@ -13,7 +13,10 @@ public class FluidBlood extends Fluid {
 	public FluidBlood() {
         super(SAMWFNames.Blood);
         this.setDensity(10);
-        this.setViscosity(1000);
+        this.setViscosity(2000);
+        this.setTemperature(310);
+        this.setDensity(2000);
+        this.setLuminosity(10);
         FluidRegistry.registerFluid(this);
 }
 
