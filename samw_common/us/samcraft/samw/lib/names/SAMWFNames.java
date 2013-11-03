ac67536d408a397822cfa7600a7dba97a2a119c1
LordIllyohs <lordillyohs@gmail.com>
2013-11-02 21:46:51 -0400

    a derp

--- samw_common/us/samcraft/samw/lib/names/SAMWFNames.java
+++ samw_common/us/samcraft/samw/lib/names/SAMWFNames.java
@@ -7,6 +7,8 @@ package us.samcraft.samw.lib.names;
 public class SAMWFNames {
 	
 	public static final String Blood = "Blood";
-	public static final String HeavyWater = "Heavy Water"; 
+	public static final String Heavy = "Heavy Water"; 
+	public static final String Pink = "Pink Water";
+	public static final String Yellow = "Yellow Water";
 
 }
