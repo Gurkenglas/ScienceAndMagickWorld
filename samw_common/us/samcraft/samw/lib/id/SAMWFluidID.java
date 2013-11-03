7399c7856709ada7f5d4359b53cf41d85927de1a
LordIllyohs <lordillyohs@gmail.com>
2013-11-02 21:47:29 -0400

    moar id's

--- samw_common/us/samcraft/samw/lib/id/SAMWFluidID.java
+++ samw_common/us/samcraft/samw/lib/id/SAMWFluidID.java
@@ -9,9 +9,13 @@ public class SAMWFluidID {
 	/* Default Ids */
     public static int BLOOD_DEFAULT = 3900;
     public static int HEAVY_DEFAULT = 3901;
+    public static int PINK_DEFAULT = 3902;
+    public static int YELLOW_DEFALT = 3903;
     
     /* Current Id */
     public static int BLOOD;
     public static int HEAVY;
+    public static int PINK;
+    public static int YELLOW;
 
 }
