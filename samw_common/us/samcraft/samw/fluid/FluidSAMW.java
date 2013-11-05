b5b5133c470219be07fad4510aacd203866ceeb8
Gurkenglas <gurkenglas@hotmail.de>
2013-11-05 22:51:17 +0100

    Adding missing class

---
+++ samw_common/us/samcraft/samw/fluid/FluidSAMW.java
@@ -0,0 +1,29 @@
+package us.samcraft.samw.fluid;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+
+public abstract class FluidSAMW extends Fluid {
+
+	public FluidSAMW(String name) {
+		super(name);
+		setFluidProperties();
+        FluidRegistry.registerFluid(this);
+        setUnlocalizedName(name);
+	}
+
+	protected abstract void onPlayerCollision(World world, int x,
+			int y, int z, EntityLivingBase entity, int meta);
+
+	public abstract void setFluidProperties();
+	
+	public abstract int colorMultiplier(IBlockAccess iblockaccess, int x, int y, int z);
+
+	public abstract int getFluidBlockID();
+	public static void setFluidBlockID(int id) {
+		throw new RuntimeException("Something tried to assign a FluidBlockID to FluidSAMW");
+	}
+}
